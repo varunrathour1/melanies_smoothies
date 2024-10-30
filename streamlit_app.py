8aa403ef404b3896c29ca4013e8f28a370aba354
varunrathour1 <varunrathour1@gmail.com>
2024-10-30 16:32:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        
     
     # Fetch data from Fruityvice API
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
     fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # Create insert statement for Snowflake
